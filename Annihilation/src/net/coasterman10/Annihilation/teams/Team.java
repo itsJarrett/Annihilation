cdf2735b6844791651a3607a12d7a92442d9bc06
coasterman10 <christian.s.farrell@gmail.com>
2013-12-14 01:30:51 -0500

    Replaced switch statement with parameter, what was I thinking

--- Annihilation/src/net/coasterman10/Annihilation/teams/Team.java
+++ Annihilation/src/net/coasterman10/Annihilation/teams/Team.java
@@ -5,7 +5,6 @@ import java.util.Set;
 
 import org.apache.commons.lang.WordUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class Team {
@@ -13,24 +12,9 @@ public class Team {
     private final String prefix;
     private final Set<String> playerNames = new HashSet<String>();
 
-    public Team(String name) {
+    public Team(String name, String prefix) {
 	this.name = name;
-	switch (name) {
-	case "red":
-	    prefix = ChatColor.RED.toString();
-	    break;
-	case "yellow":
-	    prefix = ChatColor.YELLOW.toString();
-	    break;
-	case "green":
-	    prefix = ChatColor.GREEN.toString();
-	    break;
-	case "blue":
-	    prefix = ChatColor.BLUE.toString();
-	    break;
-	default:
-	    prefix = ChatColor.DARK_PURPLE.toString();
-	}
+	this.prefix = prefix;
     }
 
     public void addPlayer(String name) {
--- Annihilation/src/net/coasterman10/Annihilation/teams/TeamManager.java
+++ Annihilation/src/net/coasterman10/Annihilation/teams/TeamManager.java
@@ -3,19 +3,22 @@ package net.coasterman10.Annihilation.teams;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.ChatColor;
+
 import net.coasterman10.Annihilation.Annihilation;
 import net.coasterman10.Annihilation.commands.TeamCommand;
 
 public class TeamManager {
     private final List<Team> teams = new ArrayList<Team>();
-    private final Team lobby = new Team("lobby");
+    private final Team lobby = new Team("lobby",
+	    ChatColor.DARK_PURPLE.toString());
 
     public TeamManager(Annihilation plugin) {
 	new TeamCommand(plugin, this);
-	teams.add(new Team("red"));
-	teams.add(new Team("blue"));
-	teams.add(new Team("green"));
-	teams.add(new Team("yellow"));
+	teams.add(new Team("red", ChatColor.RED.toString()));
+	teams.add(new Team("blue", ChatColor.BLUE.toString()));
+	teams.add(new Team("green", ChatColor.GREEN.toString()));
+	teams.add(new Team("yellow", ChatColor.YELLOW.toString()));
     }
 
     public boolean areFriendly(String p1, String p2) {
